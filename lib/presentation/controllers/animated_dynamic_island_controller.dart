594e4622ae5ac8a28b94dc906cfff3b904215df9
Hercules <hercules.silva@dcx.ufpb.br>
2022-10-29 19:29:35 -0300

    added airpod island

--- lib/presentation/controllers/animated_dynamic_island_controller.dart
+++ lib/presentation/controllers/animated_dynamic_island_controller.dart
@@ -1,42 +1,28 @@
 import 'package:flutter/foundation.dart';
 
 class AnimatedDynamicIsland extends ChangeNotifier {
-  double topContainerHeight = 0.0;
-  double bottomContainerHeight = 0.0;
-  double arrowPosition = -200.0;
-  bool _startedAnimation = false;
+  bool isDefaultIcon = false;
 
-  startAnimation() async {
-    if (_startedAnimation) return;
-    _startedAnimation = true;
-    await _animateArrow();
-    _animateBottom();
-    _animateTop();
+  bool _showIcons = true;
+  double opacity = 0.0;
+
+  changeButton() async {
+    isDefaultIcon = !isDefaultIcon;
+
+    if (isDefaultIcon) {
+      hideIcons();
+    } else {
+      showIcons();
+    }
   }
 
-  _animateArrow() async {
-    await Future.delayed(const Duration(seconds: 0));
-    arrowPosition = 200.0;
+  showIcons() {
+    opacity = 1.0;
     notifyListeners();
   }
 
-  _animateBottom() async {
-    await Future.delayed(const Duration(milliseconds: 800));
-    bottomContainerHeight = 195 + 100;
-    notifyListeners();
-  }
-
-  _animateTop() async {
-    await Future.delayed(const Duration(milliseconds: 1600));
-    topContainerHeight = 195;
-    notifyListeners();
-  }
-
-  restartAnimation() {
-    _startedAnimation = false;
-    topContainerHeight = 80.0;
-    bottomContainerHeight = 20.0;
-    arrowPosition = 0.0;
+  hideIcons() {
+    opacity = 0.0;
     notifyListeners();
   }
 }
--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -17,32 +17,78 @@ class _HomeViewState extends State<HomeView> {
 
   Widget _buildBody() {
     return Consumer<AnimatedDynamicIsland>(
-      builder: (context, value, child) {
-        return Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        builder: (context, controller, child) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildDynamicIslandScaffold(controller),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                buildDynamicIslandScaffold(),
+                InkWell(
+                  onTap: () => controller.changeButton(),
+                  child: Container(
+                      padding: EdgeInsets.all(8),
+                      color: Colors.blue,
+                      child: const Text('change island')),
+                ),
+                const SizedBox(height: 32),
               ],
             ),
-          ],
-        );
-      },
-    );
+          )
+        ],
+      );
+    });
   }
 
-  Widget buildDynamicIslandScaffold() {
+  Widget buildDynamicIslandScaffold(AnimatedDynamicIsland controller) {
     final double width = MediaQuery.of(context).size.width;
-    return Container(
+    return AnimatedContainer(
+      width: controller.isDefaultIcon ? width * 0.3 : width * 0.5,
+      height: 30,
+      duration: const Duration(milliseconds: 200),
       margin: const EdgeInsets.only(top: 16.0),
       decoration: BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.circular(15),
       ),
-      child: Container(width: width / 3.5, height: 30),
+      child: airpods(controller),
+    );
+  }
+
+  Widget center() {
+    final double width = MediaQuery.of(context).size.width;
+    return Container(width: width / 3.5, height: 30);
+  }
+
+  Widget airpods(AnimatedDynamicIsland controller) {
+    return Row(
+      children: [
+        AnimatedOpacity(
+          opacity: controller.opacity,
+          duration: const Duration(milliseconds: 100),
+          child: const Padding(
+            padding: EdgeInsets.only(left: 4.0),
+            child: Icon(Icons.headphones_rounded, color: Colors.white),
+          ),
+        ),
+        const Spacer(),
+        AnimatedOpacity(
+          opacity: controller.opacity,
+          duration: const Duration(milliseconds: 100),
+          child: const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.circle_outlined, color: Colors.green),
+          ),
+        )
+      ],
     );
   }
 }
